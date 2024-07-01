ead56686373e2c83b815b04aa1086ed945a5c804
RobsonFe <robson_20ferreira@hotmail.com>
2024-07-01 10:09:52 -0300

    update:finalizado

--- App.py
+++ App.py
@@ -5,12 +5,14 @@ sg.theme('reddit')
 layout = [
     [sg.Text('E-mail'), sg.Input(key='email')],
     [sg.Text('Senha'), sg.Input(key='senha', password_char='*')],
-    [sg.FolderBrowse('Escolher Pasta de Anexos', target='input_anexos'),sg.Input(key='input_anexos')],
-    [sg.FolderBrowse('Escolher Pasta de Planilhas', target='input_planilhas'),sg.Input(key='input_planilhas')],
+    [sg.FolderBrowse('Escolher Pasta de Anexos',
+                     target='input_anexos'), sg.Input(key='input_anexos')],
+    [sg.FolderBrowse('Escolher Pasta de Planilhas',
+                     target='input_planilhas'), sg.Input(key='input_planilhas')],
     [sg.Button('Salvar')],
 ]
 
-janela = sg.Window('Principal', layout = layout)
+janela = sg.Window('Principal', layout=layout)
 
 while True:
     event, values = janela.read()
@@ -26,3 +28,4 @@ while True:
         print(f'A senha digitada foi {senha}')
         print(f'O caminho do anexo digitado foi {anexos}')
         print(f'O caminho da planilha digitado foi {planilha}')
+        print("Finalizado!")
